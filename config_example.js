eb5fbaddc689c6f8d76ea2861b35234088c854a5
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-02-15 14:44:11 -0800

    Add example config.js file

---
+++ config_example.js
@@ -0,0 +1,13 @@
+var config = {
+  trigger_phrase: TRIGGER_PHRASE,
+  db: {
+    url: URL_PASSWORD
+  },
+  start_time: START_TIME,
+  main_page_id: MAIN_PAGE_ID,
+  page_ids: [PAGE_ID_1, PAGE_ID_2],
+  user_access_token: USER_ACCESS_TOKEN,
+  page_access_token: PAGE_ACCESS_TOKEN
+};
+
+module.exports = config;
